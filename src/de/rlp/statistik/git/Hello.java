de70604e0d25319099e105ed374fbf77f0ac15f1
TorstenKnopp <torsten.knopp@statistik.rlp.de>
2017-11-16 15:39:16 +0100

    grüße auch an koblenz

--- src/de/rlp/statistik/git/Hello.java
+++ src/de/rlp/statistik/git/Hello.java
@@ -3,7 +3,7 @@ package de.rlp.statistik.git;
 public class Hello {
     
     public static void main(String[] args) {
-        System.out.println("Hello people in Bad Ems!");
+        System.out.println("Hello people in Bad Ems und der Umgebung von Koblenz!");
     }
 
 }
