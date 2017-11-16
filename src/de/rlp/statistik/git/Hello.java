de23fd55d658574d607a109fa48e97d2e47e9601
TorstenKnopp <torsten.knopp@statistik.rlp.de>
2017-11-16 15:21:39 +0100

    People in bad Ems

--- src/de/rlp/statistik/git/Hello.java
+++ src/de/rlp/statistik/git/Hello.java
@@ -3,7 +3,7 @@ package de.rlp.statistik.git;
 public class Hello {
     
     public static void main(String[] args) {
-        System.out.println("Hello to so many people!");
+        System.out.println("Hello people in Bad Ems!");
     }
 
 }
